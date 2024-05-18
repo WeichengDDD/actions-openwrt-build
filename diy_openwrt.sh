fa32885a4599f8b0fc5129a7bdbd89d8ea03079b
Xipeng.Liu <Xipeng_Liu@foxmail.com>
2024-05-18 12:47:46 +0800

    feat: 工作流初始化

---
+++ diy_openwrt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# Modify default theme
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Modify hostname
+#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
